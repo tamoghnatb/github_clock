aa9c7e03740a235cd17f8c46e706d123ede29e88
amish1999 <47895005+amish1999@users.noreply.github.com>
2019-10-14 21:36:04 +0530

    clock updation of setting code for minutes

--- script.js
+++ script.js
@@ -18,34 +18,12 @@ var showCurrentTime = function()
     var seconds = currentTime.getSeconds();
     var meridian = "AM";
 
-    // Set hours
-	  if (hours >= noon)
-	  {
-		  meridian = "PM";
-	  }
-
-	  if (hours > noon)
-	  {
-		  hours = hours - 12;
-	  }
-
-    // Set Minutes
-    if (minutes < 10)
-    {
-        minutes = "0" + minutes;
-    }
-
-    // Set Seconds
-    if (seconds < 10)
-    {
-        seconds = "0" + seconds;
-    }
-
-    // put together the string that displays the time
-    var clockTime = hours + ':' + minutes + ':' + seconds + " " + meridian + "!";
-
-    clock.innerText = clockTime;
-};
+    //add here
+	
+	
+	
+	
+	
 
 // Getting the clock to increment on its own and change out messages and pictures
 var updateClock = function()
